3e9b23d2d10759e8dfe621773d253fc296c5391d
axer4 <79149727+axer4@users.noreply.github.com>
2021-10-16 17:54:50 +0300

    1

---
+++ src/App.js
@@ -0,0 +1,12 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Привіт, світ! Життя химерне!
+    </div>
+  );
+}
+
+export default App;
